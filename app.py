e9f75f43bace81088cfd5d070ca76405a43403f0
Ridwan Bagus <ridwansogan@gmail.com>
2019-11-03 16:40:10 +0700

    Flask Beginner

---
+++ app.py
@@ -0,0 +1,14 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+API_URL = 'http://localhost:8000/api/v1'
+
+@app.route('/')
+def index():
+    roles = requests.get(f'{API_URL}/roles')
+    roles = roles.json()
+    print(roles)
+
+    return render_template('index.html', roles=roles)
